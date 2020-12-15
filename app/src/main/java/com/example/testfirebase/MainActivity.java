85adfe591ec16e5d7bb0d1a0ac92a5cc8d8f88bc
Anh Vu <thav06101999@gmail.com>
2020-12-15 20:01:31 +0700

    init

---
+++ app/src/main/java/com/example/testfirebase/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.testfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText num,code;
+    Button confirm,btn;
+    String phoneNumber = "+84346245763";
+    //String verificationIds;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        num = findViewById(R.id.editTextNumber);
+        confirm = findViewById(R.id.button);
+        btn = findViewById(R.id.button2);
+        code = findViewById(R.id.editTextNumber2);
+
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number = num.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    num.setError("Valid number is required");
+                    num.requestFocus();
+                    return;
+                }
+
+
+                Intent intent = new Intent(MainActivity.this, VerifyPhone.class);
+                intent.putExtra("phonenumber", phoneNumber);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void enableUserManuallyInputCode() {
+    }
+
+}
